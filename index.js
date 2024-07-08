9f0ccf189600d020b94d912de794082adcab906c
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-08 10:14:04 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ console.error(`Error: ${error}`);
 
 const showMenu = () => {
 console.clear(); // Limpiar la consola
-console.log('[31m        DDoS attack     ');
+console.log('[41m        DDoS attack     ');
 console.log('⭐️ Desarrollado por Keiji821');
 console.log('[36m ⸂⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⸃');
 console.log('[32m ︳1. Iniciar ataque DDoS.               ︳');
