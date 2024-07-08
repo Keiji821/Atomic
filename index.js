47acfaf6edb78e965d5922b1f477f865f8fa6850
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-08 08:35:39 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -87,14 +87,14 @@ const showMenu = () => {
 console.clear(); // Limpiar la consola
 console.log(`${colors.fg.green}Bienvenido al Termux Discord Bot!${colors.reset}`);
 console.log(`${colors.fg.yellow}Desarrollado por Keiji821${colors.reset}`);
-console.log(`${colors.fg.cyan}⸂⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⸃${colors.reset}`);
-console.log(`${colors.fg.green}1. Iniciar ataque DDoS${colors.reset}`);
-console.log(`${colors.fg.cyan}2. Actualizar código desde GitHub${colors.reset}`);
-console.log(`${colors.fg.yellow}3. Configurar conexiones simultaneas${colors.reset}`);
-console.log(`${colors.fg.green}4. Aumentar potencia del ataque${colors.reset}`);
-console.log(`${colors.fg.red}5. Salir${colors.reset}`);
-console.log(`${colors.fg.cyan}⸌⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⸍${colors.reset}`);
-rl.setPrompt(`${colors.fg.bright} ➤ ${colors.reset}`); // Establecer el texto de la casilla "Opción: "
+console.log('[36m⸂⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⸃ [0m`);
+console.log('[36m1. Iniciar ataque DDoS [0m`);
+console.log('[36m2. Actualizar código desde GitHub [0m`);
+console.log('[36m3. Configurar conexiones simultaneas [0m`);
+console.log('[36m4. Aumentar potencia del ataque [0m`);
+console.log('[36m5. Salir [0m`);
+console.log('[36m⸌⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⸍${colors.reset}`);
+rl.setPrompt('[36m ➤ [0m`); // Establecer el texto de la casilla "Opción: "
 rl.prompt(); // Mostrar la casilla "Opción:"
 };
 
