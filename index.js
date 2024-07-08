0bddd8aca0c59a853ce0be67dd4b902f31bee8f0
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-07-08 09:22:04 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -87,13 +87,13 @@ const showMenu = () => {
 console.clear(); // Limpiar la consola
 console.log('[31m        DDoS attack     ');
 console.log('⭐️ Desarrollado por Keiji821');
-console.log('[36m ⸂⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⸃');
+console.log('[36m ⸂⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⸃');
 console.log('[36m ︳1. Iniciar ataque DDoS');
 console.log('[36m ︳2. Actualizar código desde GitHub');
 console.log('[36m ︳3. Configurar conexiones simultaneas');
 console.log('[36m ︳4. Aumentar potencia del ataque');
 console.log('[36m ︳5. Salir');
-console.log('[36m ⸌⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⸍');
+console.log('[36m ⸌⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⸍');
 rl.setPrompt('[36m ➤ '); // Establecer el texto de la casilla "Opción: "
 rl.prompt(); // Mostrar la casilla "Opción:"
 };
